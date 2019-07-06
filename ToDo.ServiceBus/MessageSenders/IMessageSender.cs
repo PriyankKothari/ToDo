245d39610ec981da521d9a6d7d3b92fe0098164f
Priyank Kothari <Priyank.K@debitsuccess.com>
2019-07-06 14:38:44 +1200

    Added ServiceBus project to send Messages to Microsoft Azure Service Bus.

---
+++ ToDo.ServiceBus/MessageSenders/IMessageSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ToDo.ServiceBus.MessageSenders
+{
+    public interface IMessageSender
+    {
+        Task SendMessage(string messageBody);
+    }
+}
---
+++ ToDo.ServiceBus/MessageSenders/MessageSender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace ToDo.ServiceBus.MessageSenders
+{
+    public class MessageSender : IMessageSender
+    {
+        private readonly IQueueClient _queueClient;
+
+        public MessageSender(string serviceBusConnectionString, string queueName)
+        {
+            this._queueClient = new QueueClient(serviceBusConnectionString, queueName);
+        }
+
+        public async Task SendMessage(string messageBody)
+        {
+            try
+            {
+                await this._queueClient.SendAsync(new Message(Encoding.UTF8.GetBytes(messageBody)));
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+        }
+    }
+}
